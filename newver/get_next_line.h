ac2b8df0aaa0637d96b4e2930a0e9974054a47d6
tfolly <tfolly@student.42.fr>
2016-01-05 14:33:39 +0100

    recommencer avec liste chainee

--- newver/get_next_line.h
+++ newver/get_next_line.h
@@ -1,12 +1,16 @@
 #ifndef GET_NEXT_LINE
 # define GET_NEXT_LINE
 # define BUF_SIZE 32
+# define FD_SIZE 100
 # include "libft.h"
 # include "fcntl.h"
 int	get_next_line(int const fd, char **line);
 
-
-
-
+typedef struct			s_stock
+{
+	char				*str;
+	int					fd;
+	struct s_stock		*next;
+}						t_stock;
 
 #endif
