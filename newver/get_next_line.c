e26a9988d6a652ba6ebdd2a5b0aa3866ce09e538
tfolly <tfolly@student.42.fr>
2016-01-05 19:11:11 +0100

    soir

--- newver/get_next_line.c
+++ newver/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tfolly <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/05 14:34:52 by tfolly            #+#    #+#             */
-/*   Updated: 2016/01/05 17:32:26 by tfolly           ###   ########.fr       */
+/*   Updated: 2016/01/05 18:19:55 by tfolly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,46 @@
 
 static t_stock		*check_fd(const int fd, t_stock *stock)
 {
+	ft_putendl("appel de check_fd");
 	if (!stock)
 	{
 		if (!(stock = (t_stock*)malloc(sizeof(t_stock))))
 			return (NULL);
 		stock->fd = fd;
+		stock->str = ft_strdup("");
 		return (stock);
 	}
-	while (stock->fd != fd && stock->next)
+	while ((stock->fd != fd) && stock->next)
 		stock = stock->next;
 	if (stock->fd == fd)
+	{
 		return (stock);
+	}
 	else
 	{
 		if (!(stock->next = (t_stock*)malloc(sizeof(t_stock))))
 			return (NULL);
 		stock = stock->next;
 		stock->fd = fd;
+		stock->str = ft_strdup("");
 		return (stock);
-		
 	}
 }
 
-static int		bufcpy(char **line, char **tmp)
+static int		bufcpy(char **line, t_stock *stock)
 {
+	ft_putendl("appel de bufcpy");
 	int		n;
 	int		i;
 	char	*save;
 	char	*addr;
+	char	*tmp;
 
-	n = (int)(ft_strchr(*tmp, '\n') - *tmp);
-	*line = ft_strndup(*tmp, n);
-	save = *tmp;
-	*tmp += n;
+	tmp = stock->str;
+	n = (int)(ft_strchr(tmp, '\n') - *tmp);
+	*line = ft_strndup(tmp, n);
+	save = tmp;
+	tmp += n;
 	i = 0;
 	while (i < n) //repenser la maniere de mettre a jour tmp car ca c pas correct
 	{
@@ -60,12 +67,20 @@ static int		bufcpy(char **line, char **tmp)
 
 static int		fill_tmp(t_stock *stock)
 {
+	ft_putendl("appel de fill_tmp");
 	char	*tmp;
 	char	buf[BUF_SIZE];
 	int		nbr;
 	char	*save;
+	int		i;
 
 	nbr = 1;
+	i = 0;
+	while (i < BUF_SIZE)
+	{
+		buf[i] = 0;
+		i++;
+	}
 	tmp = stock->str;
 	while (!(ft_strchr(buf, '\n') || ft_strchr(buf, -1) || nbr == 0))
 	{
@@ -75,6 +90,7 @@ static int		fill_tmp(t_stock *stock)
 		tmp = ft_strcpy(tmp, save);
 		tmp = ft_strcat(tmp, (const char*)buf);
 		ft_memdel((void**)&save);
+		ft_putendl(tmp);
 	}
 	if (nbr == -1)
 		return (-1);
@@ -86,15 +102,21 @@ static int		fill_tmp(t_stock *stock)
 int				get_next_line(int const fd, char **line)
 {
 	static t_stock	*stock;
-	char			*tmp;
 	int				status;
+	t_stock			*tmp;
 
-	stock = check_fd(fd, stock);
-	tmp = stock->str;
-	if (!(ft_strchr(tmp, '\n') || ft_strchr(tmp, -1)))
-		status = fill_tmp(stock);
-	bufcpy(line, &tmp);
+	if (!(tmp = check_fd(fd, stock)))
+		return (-1);
+	if (!(ft_strchr(tmp->str, '\n') || ft_strchr(tmp->str, -1)))
+	{
+		status = fill_tmp(tmp);
+	}
+	ft_putstr("stock->str : ");
+	ft_putendl(tmp->str);
+	bufcpy(line, tmp);
 	// il faut mettre a jour str dans la liste chainee
 	// penser au cas ou on a copier plsr \n
 	return (1);
 }
+
+//manipuler chaine de char meilleur que manip stock directement car pas dupdate!!!
