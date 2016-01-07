e5d65ca3dd7bef05ff8c17456401614cb62e36bf
tfolly <tfolly@student.42.fr>
2016-01-07 18:22:35 +0100

    20 : fix filltmp

--- newver/get_next_line.c
+++ newver/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tfolly <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/05 14:34:52 by tfolly            #+#    #+#             */
-/*   Updated: 2016/01/07 16:32:42 by tfolly           ###   ########.fr       */
+/*   Updated: 2016/01/07 18:20:49 by tfolly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ static t_stock		*check_fd(const int fd, t_stock *stock, t_stock **save)
 	}
 }
 
-static int		bufcpy(char **line, t_stock *stock)
+static int		bufcpy(char **line, t_stock *stock) //revoir tte cette fonction c ici quil y a des bugs
 {
 	//ft_putendl("buffcpy");
 	int		n;
@@ -56,12 +56,14 @@ static int		bufcpy(char **line, t_stock *stock)
 		return (0);
 	}
 	tmp = stock->str;
+//	ft_putstr("stock : ");
+//	ft_putendl(stock->str);
 	n = (int)(ft_strchr(tmp, '\n') - tmp);
-	ft_putnbr(n);
-	ft_putendl("");
+//	ft_putnbr(n);
+//	ft_putendl("");
 	n = ((unsigned int)n > (unsigned int)ft_strlen(tmp) + 1) ? ft_strlen(tmp) : n;
-	ft_putnbr(n);
-	ft_putendl("");
+//	ft_putnbr(n);
+//	ft_putendl("");
 	*line = ft_strndup(tmp, n);
 	save = tmp;
 	tmp += n;
@@ -74,7 +76,9 @@ static int		bufcpy(char **line, t_stock *stock)
 		i++;
 		
 	}
-	stock->str = save + i + 1;
+	stock->str = tmp + 1;
+//	ft_putstr("stock : ");
+//	ft_putendl(stock->str);
 	return (1);
 }
 
@@ -87,28 +91,34 @@ static int		fill_tmp(t_stock *stock)
 	char	*save;
 	int		i;
 
+	if (stock->status == 0)
+		return (0);
 	nbr = BUF_SIZE;
 	i = 0;
-//	while (i < BUF_SIZE) //est ce que buff est donnee vide
-//	{
-//		buf[i] = 0;
-//		i++;
-//	}
-	tmp = stock->str;
-	while (!(ft_strchr(buf, '\n') || ft_strchr(buf, -1) || nbr == 0) && nbr == BUF_SIZE)
+	while (i < BUF_SIZE) //est ce que buff est donnee vide // utiliser memdel a la place
 	{
+		buf[i] = 0;
+		i++;
+	}
+	tmp = stock->str;
+	while ((!ft_strchr(tmp, '\n') || nbr == 0) && nbr == BUF_SIZE)
+	{
+		//ft_putendl("while filltmp");
 		nbr = read(stock->fd, buf, BUF_SIZE);
 		if (nbr < BUF_SIZE)
 			stock->status = 0;
 		if (nbr == -1)
 			return (nbr);
 		save = tmp;
-		tmp = (char*)ft_memalloc(ft_strlen(tmp) + BUF_SIZE);
+		tmp = (char*)ft_memalloc(ft_strlen(tmp) + BUF_SIZE + 1);
 		tmp = ft_strcpy(tmp, save);
 		tmp = ft_strcat(tmp, (const char*)buf);
+		tmp[ft_strlen(save) + nbr] = '\0';
 		//if (save)
 		//	ft_memdel((void**)&save);
 		stock->str = tmp;
+		//ft_putstr(" fill stock : ");
+		//ft_putendl(stock->str);
 	}
 	if (nbr == -1)
 		return (-1);
@@ -127,13 +137,15 @@ int				get_next_line(int const fd, char **line)
 		return (-1);
 	if (!save)
 		save = (t_stock*)ft_memalloc(sizeof(t_stock));
+	if (!save)
+		return (-1);
 	stock = save;
 	if (!(stock = check_fd(fd, stock, &save)))
 		return (-1);
-	if (!(ft_strchr(stock->str, '\n') || ft_strchr(stock->str, -1)) && stock->status == 1)
+	if (!(ft_strchr(stock->str, '\n')))// && stock->status == 1)
 		stock->status = fill_tmp(stock);
 	//ft_putendl(stock->str);
-	if (stock->status != 1)
+	if (stock->status == -1)
 		return (stock->status);
 	bufcpy(line, stock);
 	// il faut mettre a jour str dans la liste chainee
--- newver/get_next_line.h
+++ newver/get_next_line.h
@@ -1,6 +1,6 @@
 #ifndef GET_NEXT_LINE
 # define GET_NEXT_LINE
-# define BUF_SIZE 32
+# define BUF_SIZE 10
 # include "libft.h"
 # include "fcntl.h"
 int	get_next_line(int const fd, char **line);
